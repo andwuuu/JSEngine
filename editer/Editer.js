de06415bbedc92aeb0f79f85b9e77b85776204d5
andwuuu <160696040+andwuuu@users.noreply.github.com>
2024-02-24 01:33:46 +0900

    2’

--- editer/Editer.js
+++ editer/Editer.js
@@ -87,7 +87,7 @@ class Editer{
       this.ctx.lineTo(this.canvas.width,y);
     }
 
-    this.ctx.strokeStyle = "#006600";
+    this.ctx.strokeStyle = "#ddffff";
     this.ctx.lineWidth = 0.1;
     this.ctx.stroke();
   }
