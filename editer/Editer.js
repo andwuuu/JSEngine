ac483d0c4319b943339755eed2100a1f70394e46
andwuuu <160696040+andwuuu@users.noreply.github.com>
2024-02-24 00:53:30 +0900

    すまん

--- editer/Editer.js
+++ editer/Editer.js
@@ -168,8 +168,8 @@ class Entity{
     }else{
       ctx.beginPath();
       ctx.arc(this.posX,this.posY,this.size,0,2*Math.PI);
-      ctx.strokeStyle = "red";
-      ctx.fillStyle = "red";
+      ctx.strokeStyle = "aqua";
+      ctx.fillStyle = "aqua";
       ctx.lineWidth = 1;
       ctx.fill();
       ctx.stroke();
